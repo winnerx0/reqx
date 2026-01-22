6defee93cf7d134339d697bdedbb573c653c5e0e
winner <eragbonikpeya@gmail.com>
2026-01-22 09:19:27 +0100

    refactor: Add response body close

--- internal/utils/request.go
+++ internal/utils/request.go
@@ -11,41 +11,43 @@ import (
 var client = &http.Client{}
 
 func SendRequest(request Request, silent bool, name string) error {
-			bodyBytes, err := json.Marshal(request.Body)
+	bodyBytes, err := json.Marshal(request.Body)
 
-			if err != nil {
-				return err
-			}
+	if err != nil {
+		return err
+	}
 
-			jsonData := bytes.NewReader(bodyBytes)
+	jsonData := bytes.NewReader(bodyBytes)
 
-			req, err := http.NewRequest(string(request.Method), request.Url, jsonData)
+	req, err := http.NewRequest(string(request.Method), request.Url, jsonData)
 
-			if err != nil {
-				return err
-			}
+	if err != nil {
+		return err
+	}
 
-			for k, v := range request.Headers {
+	for k, v := range request.Headers {
 
-				req.Header.Add(k, v)
-			}
+		req.Header.Add(k, v)
+	}
 
-			response, err := client.Do(req)
+	response, err := client.Do(req)
 
-			if err != nil {
-				return err
-			}
+	if err != nil {
+		return err
+	}
 
-			if silent {
-				fmt.Println("Name: " + request.Name + "\t Status: " + response.Status + "\n")
-			} else {
+	defer response.Body.Close()
 
-				responsebytes, err := io.ReadAll(response.Body)
+	if silent {
+		fmt.Println("Name: " + request.Name + "\t Status: " + response.Status + "\n")
+	} else {
 
-				if err != nil {
-					return err
-				}
-				fmt.Println("Name: " + request.Name + "\n\n" + string(responsebytes) + "\n")
-			}
-			return nil
+		responsebytes, err := io.ReadAll(response.Body)
+
+		if err != nil {
+			return err
+		}
+		fmt.Println("Name: " + request.Name + "\n\n" + string(responsebytes) + "\n")
+	}
+	return nil
 }
